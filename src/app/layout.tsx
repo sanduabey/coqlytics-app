11613c42a8872d2d2846c861c80c1439a7851992
Sandu Abeywickrama <s@abey.one>
2023-07-09 17:41:21 +1000

    navbar added with styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-import './globals.css'
 import type { Metadata } from 'next'
-// import { Inter } from 'next/font/google'
 import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
-
-// const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Coqlytics',
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,20 +3,20 @@ import Link from 'next/link'
 export default function NavBar() {
   return (
     <nav>
-      <ul className="flex gap-4 text-center">
-        <li>
+      <ul className="flex gap-4">
+        <li className="text-blue-300 hover:underline">
           <Link href="chikn"> Chikn</Link>
         </li>
-        <li>
+        <li className="text-blue-300 hover:underline">
           <Link href="roostr">Roostr</Link>
         </li>
-        <li>
+        <li className="text-blue-300 hover:underline">
           <Link href="farmland"> FarmLand</Link>
         </li>
-        <li>
+        <li className="text-blue-300 hover:underline">
           <Link href="blueprint">BluePrint </Link>
         </li>
-        <li>
+        <li className="text-blue-300 hover:underline">
           <Link href="item"> Item </Link>
         </li>
       </ul>
