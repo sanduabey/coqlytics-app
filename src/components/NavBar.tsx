4de12bfb151c82eddc0cc1f97c79ec877a27afe9
Sandu Abeywickrama <s@abey.one>
2023-07-10 10:52:23 +1000

    cleaned up a bit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,26 +2,26 @@ import Link from 'next/link'
 
 export default function NavBar() {
   return (
-    <nav className="flex flex-row">
+    <nav className="flex">
       <div className="basis-1/5 justify-start text-purple-400">
         {' '}
         <Link href="/">Coqlytics.xyz</Link>{' '}
       </div>
 
-      <ul className="basis-4/5 flex gap-5 justify-end">
-        <li className="text-blue-300 hover:underline">
+      <ul className="basis-4/5 flex gap-5 justify-end text-blue-300">
+        <li className="hover:underline">
           <Link href="chikn"> Chikn</Link>
         </li>
-        <li className="text-blue-300 hover:underline">
+        <li className="hover:underline">
           <Link href="roostr">Roostr</Link>
         </li>
-        <li className="text-blue-300 hover:underline">
+        <li className="hover:underline">
           <Link href="farmland"> FarmLand</Link>
         </li>
-        <li className="text-blue-300 hover:underline">
+        <li className="hover:underline">
           <Link href="blueprint">BluePrint </Link>
         </li>
-        <li className="text-blue-300 hover:underline">
+        <li className="hover:underline">
           <Link href="item"> Item </Link>
         </li>
       </ul>
