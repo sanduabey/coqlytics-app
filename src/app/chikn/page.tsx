a49a11304c575906058c503caf217cafd3ef45b0
Sandu Abeywickrama <s@abey.one>
2023-07-12 10:05:39 +1000

    chikn vol chart with dummy data added

--- src/app/chikn/page.tsx
+++ src/app/chikn/page.tsx
@@ -4,6 +4,7 @@ import PageHeading from '@/components/PageHeading'
 import SoldItem from '@/components/chikn/SoldChikn'
 import RecentChiknSales from '@/components/chikn/RecentChiknSales'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import ChiknVolChart from '@/components/chikn/ChiknVolChart'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +12,9 @@ export default function ChiknPage() {
   return (
     <QueryClientProvider client={queryClient}>
       <PageHeading> Chikn Marketplace Stats </PageHeading>
+      <section className="bg-gray-200 flex justify-center">
+        <ChiknVolChart />
+      </section>
       <section className="bg-chiknpurple">
         <PageHeading>Latest Chikns Sales</PageHeading>
         <RecentChiknSales />
