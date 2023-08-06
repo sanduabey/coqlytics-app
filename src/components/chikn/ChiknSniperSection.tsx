7c51060c9c64a6ef256c47c3d66cf425a57af976
Sandu Abeywickrama <s@abey.one>
2023-08-06 11:45:14 +1000

    moved SniperChikn content to a seperate component

--- src/components/chikn/ChiknSniperSection.tsx
+++ src/components/chikn/ChiknSniperSection.tsx
@@ -1,104 +1,11 @@
-import { numberWithCommas } from '@/utils/helpers'
-import ForSaleChikn from './ForSaleChikn'
-import { useQuery } from '@tanstack/react-query'
 import { BaseSyntheticEvent, useState } from 'react'
-
-type ChiknForSaleType = {
-  token: number
-  kg: number
-  salePrice: number
-  eggPerDay: number
-  lastClaimedEgg: Date
-  feedAccumulated: number
-  feedAccumulatedInAVAX: number
-  unclaimedEgg: number
-  unclaimedEggInAVAX: number
-  baseChiknValueInAVAX: number
-  AVAXperKG: number
-  daysToBreakEvenFullValue: number
-  daysToBreakEvenBaseValue: number
-  head: string
-  neck: string
-  torso: string
-  feet: string
-  tail: string
-  body: string
-  trim: string
-  background: string
-  _numOfTraits: number
-  rank: string
-  rarity: string
-  score: number
-}
-
-const getBestForSaleChikns = async (
-  sortStrategy: string,
-  maxPriceAVAX: string
-) => {
-  const response = await fetch(
-    `${process.env.HOST}/api/chikn/sniper?sort=${sortStrategy}&maxPrice=${maxPriceAVAX}`
-  )
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch best for sale Chikns')
-  }
-
-  const _response = await response.json()
-
-  // console.log(_response)
-
-  return _response.data
-}
+import SniperChiknList from './SniperChiknList'
 
 const ChiknSniperSection = () => {
   const [selectedSortStrategy, setSelectedSortStrategy] =
     useState('chiknBaseValue')
 
   const [selectedMaxPrice, setSelectedMaxPrice] = useState('any')
-
-  const { isLoading, isError, data, error, refetch } = useQuery({
-    queryKey: ['chiknSniper', selectedSortStrategy, selectedMaxPrice],
-    queryFn: () => getBestForSaleChikns(selectedSortStrategy, selectedMaxPrice),
-  })
-
-  if (isError) return <div>failed to load</div>
-  if (isLoading)
-    return <div className="flex justify-center text-white p-6">loading...</div>
-
-  const forSaleChiknContent = data.map(
-    (item: ChiknForSaleType, index: number) => {
-      const forSaleChiknData = {
-        index: index + 1,
-        image: `https://api.chikn.farm/api/chikn/thumb/${item.token}`,
-        tokenId: item.token,
-        price: item.salePrice,
-        kg: item.kg,
-        rarity: item.rarity,
-        head: item.head,
-        neck: item.neck,
-        torso: item.torso,
-        feet: item.feet,
-        tail: item.tail,
-        body: item.body,
-        trim: item.trim,
-        background: item.background,
-        numOfTraits: item._numOfTraits,
-        score: item.score,
-        rank: item.rank,
-        eggPerDay: item.eggPerDay,
-        unclaimedEgg: item.unclaimedEgg.toFixed(2),
-        unclaimedEggInAVAX: item.unclaimedEggInAVAX.toFixed(2),
-        feedAccumulated: numberWithCommas(item.feedAccumulated),
-        feedAccumulatedInAVAX: item.feedAccumulatedInAVAX.toFixed(2),
-        baseChiknValueInAVAX: item.baseChiknValueInAVAX.toFixed(2),
-        AVAXperKG: item.AVAXperKG.toFixed(2),
-        daysToBreakEvenFullValue: item.daysToBreakEvenFullValue.toFixed(0),
-        daysToBreakEvenBaseValue: item.daysToBreakEvenBaseValue.toFixed(0),
-      }
-
-      return <ForSaleChikn key={item.token} chiknData={forSaleChiknData} />
-    }
-  )
 
   const sortStrategyChangeHandler = (event: BaseSyntheticEvent) => {
     let selectedOption = event.target.value
@@ -166,7 +73,10 @@ const ChiknSniperSection = () => {
           </div>
         </div>
 
-        <ul className="p-3">{forSaleChiknContent}</ul>
+        <SniperChiknList
+          sortStrategy={selectedSortStrategy}
+          maxPriceAVAX={selectedMaxPrice}
+        />
       </div>
     </section>
   )
