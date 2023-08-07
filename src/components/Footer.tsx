48d3b4ee876e65fa3e5c9a2222a374309ee74423
Sandu Abeywickrama <s@abey.one>
2023-08-07 21:14:31 +1000

    footer link fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,11 @@
 export default function Footer() {
-  return <p className="text-gray-400"> Built by @Vktor</p>
+  return (
+    <p className="text-gray-400">
+      {' '}
+      Built by{' '}
+      <a href="https://twitter.com/dr_vktor" target="_blank">
+        @Vktor
+      </a>{' '}
+    </p>
+  )
 }
