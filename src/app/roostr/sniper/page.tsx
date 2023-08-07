5c96ae21d467dc89ca6bf9ab46ae3542aa862a29
Sandu Abeywickrama <s@abey.one>
2023-08-07 21:17:15 +1000

    added Roostr sniper page

---
+++ src/app/roostr/sniper/page.tsx
@@ -0,0 +1,7 @@
+import PageHeading from '@/components/util-components/PageHeading'
+
+const RoostrSniperPage = () => {
+  return <PageHeading>Roostr Sniper Page</PageHeading>
+}
+
+export default RoostrSniperPage
