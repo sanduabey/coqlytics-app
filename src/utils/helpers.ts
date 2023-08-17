7a905149afc7b13c7c22ab3f3e2ee1dec117e5aa
Sandu Abeywickrama <s@abey.one>
2023-08-17 18:38:43 +1000

    minor

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -95,25 +95,3 @@ export function formatDateToDDMMMYYYY(date: Date): string {
 export function numberWithCommas(x: number) {
   return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
 }
-
-const OUTLIER_FACTOR: number = 0.8
-export function getOutlierBoundary(array: number[]): {
-  minBoundary: number
-  maxBoundary: number
-} {
-  let values = array.concat()
-
-  //sort
-  values.sort((a, b) => a - b)
-
-  let q1 = values[Math.floor(values.length / 4)]
-  let q3 = values[Math.ceil(values.length * (3 / 4))]
-
-  //inter quartile range
-  let iqr = q3 - q1
-
-  let maxValue = q3 + iqr * OUTLIER_FACTOR
-  let minValue = q1 - iqr * OUTLIER_FACTOR
-
-  return { minBoundary: minValue, maxBoundary: maxValue }
-}
